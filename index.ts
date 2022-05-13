04a4abf2025e7f32ff11a029d17f77e145c9576b
anaumchev <anaumchev@gmail.com>
2022-05-13 21:09:10 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -114,21 +114,16 @@ async function run() {
             })
             .catch((err) => {
               // TODO: Create Issue in RQCODE
-//              octokit.rest.issues.create({
-//                owner: 'anaumchev',
-//                repo: 'VDO-Patterns',
-//                title: 'Test issue'
-//              });              
+              octokit_rqcode.rest.issues.create({
+                owner: 'anaumchev',
+                repo: 'VDO-Patterns',
+                title: `Implement finding ${stig_id}`
+              });      
               new_issues += `\r\n- ${stig_id}`
               // throw err
             })
         }
-        
-        await octokit_rqcode.rest.issues.create({
-          owner: 'anaumchev',
-          repo: 'VDO-Patterns',
-          title: 'Test issue'
-        });          
+                  
         
         // post a comment about already implemented test on the STIG in RQCODE or about their need in RQCODE
         if (body.length > 19)
