5cc79e4e300f260baf2743689394a391ba592399
Feyd-Rauth <atreides.pasha@yandex.ru>
2023-04-09 13:52:38 +0300

    Change api call.

--- src/apiService.ts
+++ src/apiService.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 namespace ApiService {
   export async function getSecuritySentences(requirement: string) {
     console.log('Making call to ARQAN')
-    const response = await axios.post('http://stigsearch.softeam-rd.eu:8000/text', requirement, {
+    const response = await axios.post('http://arqan.softeam-rd.eu:7000/text', requirement, {
       headers: { 'Content-type': 'text/plain;' }
     })
     console.log('Response data: ', response.data)
@@ -11,7 +11,7 @@ namespace ApiService {
   }
 
   export async function getRecommendedStigs(requirement: string, platform: string): Promise<{ [id: string]: string }> {
-    const response = await axios.get('http://stigsearch.softeam-rd.eu:8000/stigs', {
+    const response = await axios.get('http://arqan.softeam-rd.eu:7000/stigs', {
       params: { text: requirement, t_type: 1, platform: platform }
     })
     return response.data
